12f2a0ffc26d85873248cca63ffcf24dd6437cef
ashaw916 <a.s.young530@gmail.com>
2020-04-18 21:16:17 -0700

    computer answer added, as well as wins, losses, guess count.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,47 @@
-   // Let's start by grabbing a reference to the <span> below.
-   var userText = document.getElementById("guess-text");
+//variable to hold all possible answers
+var chars = ["a", "b", "c", "d", "e", "f", "g","h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-   // Next, we give JavaScript a function to execute when onkeyup event fires.
-   document.onkeyup = function(event) {
-     userText.textContent = "You guessed " + event.key;
-   };
+//variable for wins, losses, guessesleft, guessed
+var wins = 0;
+var losses = 0;
+var guessesLeft = 100;
+var guessed = 0;
+
+// references to html locations
+var guessText = document.getElementById("guess-text");
+var answerText = document.getElementById("answer-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
+var guessedText = document.getElementById("guessed-text");
+
+// function to log user input and generate answer
+document.onkeyup = function(event) {
+
+    guessText.textContent = "You guessed " + event.key;
+    
+
+    //generating answer
+    var answer = chars[Math.floor(Math.random() * chars.length)];
+        answerText.textContent = "It was " + answer;
+
+    //log wins and losses
+    if ((guessText) === (answerText)) {
+        wins++;
+        winsText.textContent = wins;
+    }
+    
+    else {
+        losses++;
+    }
+
+    
+
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft--;
+
+
+    
+    
+};
